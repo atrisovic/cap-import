80a48a1e4343de8083da5fa80a743402ba023990
Ana Trisovic <ana.trisovic@gmail.com>
2017-10-09 16:56:06 +0200

    Added check for URLs.

--- capimport/importer.py
+++ capimport/importer.py
@@ -1,13 +1,24 @@
 import os
+import re
 import urllib
 import argparse
 
 PROTOCOL = "https:/"
-ARCHIVE = "repository/master/archive.tar.gz"
+ARCHIVE_GLAB = "repository/master/archive.tar.gz"
+ARCHIVE_GHUB = "archive/master.tar.gz"
 
 
 def verify_url(url):
-    return '/'.join([PROTOCOL, url, ARCHIVE])
+    """ verify url to find host, username and repository """
+    pattern = r"(?P<host>github\.com|gitlab\.cern\.ch)\/(?P<user>[a-zA-Z][\w.-]+)\/(?P<repo>[\w.-]+)"
+    match = re.search(pattern, url, re.IGNORECASE)
+    if not match:
+        return "The URL cannot be parsed, please provide valid URL."
+    p = match.groupdict()
+    archive = ARCHIVE_GLAB if "gitlab" in p['host'] else ARCHIVE_GHUB
+    if p['repo'].endswith(".git"): p['repo'] = p['repo'][:-4]
+    p_list = [PROTOCOL, p['host'], p['user'], p['repo'], archive]
+    return '/'.join(p_list)
 
 
 def download_repo(url, token, path):
